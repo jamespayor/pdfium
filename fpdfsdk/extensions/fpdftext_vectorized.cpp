58288807aeae0a1877ceeb4eea234f7bda58ce26
James Payor <james@payor.com.au>
2017-11-06 07:26:39 +1100

    Include text color.

--- fpdfsdk/extensions/fpdftext_vectorized.cpp
+++ fpdfsdk/extensions/fpdftext_vectorized.cpp
@@ -5,6 +5,7 @@
 #include "core/fpdfapi/font/cpdf_font.h"
 #include "core/fxcrt/fx_coordinates.h"
 #include "core/fpdftext/cpdf_textpage.h"
+#include "core/fxge/fx_dib.h"
 
 
 // Copied from elsewhere, because it's not exposed in the headers...
@@ -22,6 +23,7 @@ typedef struct _FPDF_EXT_CHARINFO_ {
   FS_MATRIX matrix;
   FS_RECTF box;
   FS_POINTF origin;
+  uint32_t color_argb;
   const char *font_name;
   float font_size;
   unsigned int unicode;
@@ -56,31 +58,6 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FPDFText_GetCharInfo(FPDF_TEXTPAGE text_page
     textpage->GetCharInfo(start_index + offset, &charinfo);
 
     FPDF_EXT_CHARINFO& out_char_info = char_info[offset];
-    
-    out_char_info.unicode = charinfo.m_Unicode;
-    out_char_info.charcode = charinfo.m_Charcode;
-    out_char_info.font_size = charinfo.m_FontSize;
-
-    if (charinfo.m_pTextObj) {
-      auto font = charinfo.m_pTextObj->GetFont();
-      if (font) {
-        out_char_info.font_name = font->GetBaseFont().c_str();
-      } else {
-        out_char_info.font_name = 0;
-      }
-    } else {
-      out_char_info.font_name = 0;
-    }
-
-    const CFX_PointF& origin = charinfo.m_Origin;
-    out_char_info.origin.x = origin.x;
-    out_char_info.origin.y = origin.y;
-
-    const CFX_FloatRect& box = charinfo.m_CharBox;
-    out_char_info.box.left = box.left;
-    out_char_info.box.bottom = box.bottom;
-    out_char_info.box.right = box.right;
-    out_char_info.box.top = box.top;
 
     const CFX_Matrix& matrix = charinfo.m_Matrix;
     out_char_info.matrix.a = matrix.a;
@@ -89,6 +66,36 @@ FPDF_EXPORT FPDF_BOOL FPDF_CALLCONV FPDFText_GetCharInfo(FPDF_TEXTPAGE text_page
     out_char_info.matrix.d = matrix.d;
     out_char_info.matrix.e = matrix.e;
     out_char_info.matrix.f = matrix.f;
+
+    const CFX_FloatRect& box = charinfo.m_CharBox;
+    out_char_info.box.left = box.left;
+    out_char_info.box.bottom = box.bottom;
+    out_char_info.box.right = box.right;
+    out_char_info.box.top = box.top;
+
+    const CFX_PointF& origin = charinfo.m_Origin;
+    out_char_info.origin.x = origin.x;
+    out_char_info.origin.y = origin.y;
+    
+    if (charinfo.m_pTextObj) {
+      uint32_t alpha = static_cast<uint32_t>((charinfo.m_pTextObj->m_GeneralState.GetFillAlpha() * 255.f) + 0.5f);
+      uint32_t rgb = charinfo.m_pTextObj->m_ColorState.GetFillRGB();
+      out_char_info.color_argb = (alpha << 24) | (FXSYS_GetRValue(rgb) << 16) | (FXSYS_GetGValue(rgb) << 8) | (FXSYS_GetBValue(rgb));
+
+      auto font = charinfo.m_pTextObj->GetFont();
+      if (font) {
+        out_char_info.font_name = font->GetBaseFont().c_str();
+      } else {
+        out_char_info.font_name = 0;
+      }
+    } else {
+      out_char_info.color_argb = 0;
+      out_char_info.font_name = 0;
+    }
+
+    out_char_info.font_size = charinfo.m_FontSize;
+    out_char_info.unicode = charinfo.m_Unicode;
+    out_char_info.charcode = charinfo.m_Charcode;
 
   }
 
