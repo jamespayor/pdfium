88940b6596119c2f046f94a096331aacf9480e3e
James Payor <james@payor.com.au>
2017-11-19 18:48:11 +1100

    Use copy instead of cp.

--- compile-and-copy.bat
+++ compile-and-copy.bat
@@ -9,8 +9,10 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 ninja -C out-debug\x86 pdfium
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cp out\x64\pdfium.dll E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x64\
-cp out\x86\pdfium.dll E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x86\
-cp out-debug\x64\pdfium.dll out-debug\x64\pdfium.dll.pdb E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x64\Debug\
-cp out-debug\x86\pdfium.dll out-debug\x86\pdfium.dll.pdb E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x86\Debug\
+copy /Y out\x64\pdfium.dll E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x64\ > nul
+copy /Y out\x86\pdfium.dll E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x86\ > nul
+copy /Y out-debug\x64\pdfium.dll E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x64\Debug\ > nul
+copy /Y out-debug\x64\pdfium.dll.pdb E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x64\Debug\ > nul
+copy /Y out-debug\x86\pdfium.dll E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x86\Debug\ > nul
+copy /Y out-debug\x86\pdfium.dll.pdb E:\Git\dot-net-draftable\src\Draftable.PDF\PDFium\x86\Debug\ > nul
 
